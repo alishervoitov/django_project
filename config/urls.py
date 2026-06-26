42a1912e3a90059daba3f9afaaa1bcc0e1ff9b5d
Voitov Alisher <voitovalisher020@gmail.com>
2026-06-26 15:09:05 +0500

    render sayti uchun bazi ozgartirishlar kiritildi

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,22 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+def create_admin_automatically(request):
+    User = get_user_model()
+    if not User.objects.filter(staff_id="admin").exists():
+        User.objects.create_superuser(
+            staff_id="AB080802",
+            password="alisher",
+            email="admin@example.com"
+        )
+        return HttpResponse("Admin akkaunt muvaffaqiyatli yaratildi! Endi bu urlni ochirib tashlasangiz bo'ladi.")
+    return HttpResponse("Admin akkaunt allaqachon mavjud.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('make-me-admin-please/', create_admin_automatically),
     path('', include('attendance.urls')),
 ]
+
