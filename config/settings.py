9a6efd85f3e0adbb3c28d8d01d3901dd2ab5a17c
Voitov Alisher <voitovalisher020@gmail.com>
2026-06-27 09:55:35 +0500

    baza eski holiga qaytdi

--- config/settings.py
+++ config/settings.py
@@ -73,14 +73,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:RHTcniqq9FcQcUrq@db.rcahtgdyilxdakcxmnmp.supabase.co:6543/postgres',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
